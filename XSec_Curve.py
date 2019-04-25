f523559c0a2c63930847f97303a8518436bb06f5
blr76 <37673419+blr76@users.noreply.github.com>
2019-04-25 14:50:23 -0500

    Update XSec_Curve.py

--- XSec_Curve.py
+++ XSec_Curve.py
@@ -6,9 +6,10 @@ import arcpy, math
 # x_sec_center_points = arcpy.GetParameterAsText(3)
 
 xSecPoints = r"E:\Trinity_River\Trinity_Cleanup\XsecTEST\T1_Transect_Points_Z_WSE.shp"
-xSecStartID = 485
-xSecEndID= 523
+xSecStartID = 123
+xSecEndID= 150
 xSecCenterPoints = r"E:\Trinity_River\Trinity_Cleanup\XsecTEST\T1_Q2002.shp"
+xSecLineLength = -200
 
 xSecPointsStart = r'in_memory\ptsStart'
 xSecPointsEnd = r'in_memory\ptsEnd'
@@ -16,6 +17,9 @@ startMaxAttributes = [0,0]
 startMinAttributes =[999999,0]
 endMaxAttributes = [0,0]
 endMinAttributes =[999999,0]
+clRowVal = []
+xSecResultsTable = arcpy.CreateTable_management("in_memory", "xSecResultsTable")
+xSecResults = r"in_memory\xsSecResults"
 
 arcpy.Select_analysis(xSecPoints, xSecPointsStart, "ID = " + str(xSecStartID))
 cursor = arcpy.da.SearchCursor(xSecPointsStart, ['POINT_X', 'POINT_Y'])
@@ -42,16 +46,32 @@ intersectX = ((yc-xc*((yd-yc)/(xd-xc)))-(ya-xa*((yb-ya)/(xb-xa))))/(((yb-ya)/(xb
 intersectY = ya-xa*((yb-ya)/(xb-xa))+intersectX*((yb-ya)/(xb-xa))
 
 intersect = [intersectX, intersectY]
-point = arcpy.Point(intersectX, intersectY)
 
+##DEBUG##
+point = arcpy.Point(intersectX, intersectY)
 pointGeometry = arcpy.PointGeometry(point)
 arcpy.CopyFeatures_management(pointGeometry, "test2")
+##DEBUG##
 
-clRowVal = []
-
-with arcpy.da.SearchCursor(xSecCenterPoints, ["SHAPE@X", "SHAPE@Y","ID"], "ID >= " + str(xSecStartID) + "AND ID <= " + str(xSecEndID)) as cursor:
+with arcpy.da.SearchCursor(xSecCenterPoints, ["ID", "SHAPE@X", "SHAPE@Y"], "ID >= " + str(xSecStartID) + "AND ID <= " + str(xSecEndID)) as cursor:
     for row in cursor:
-        clRowVal.append([row[0],row[1],row[2]])
+        m = ((intersectY - row[2]) / (intersectX - row[1]))
+        c = 1 / math.sqrt(1 + math.pow(m,2))
+        s =  m / math.sqrt(1 + math.pow(m,2))
+        clRowVal.append([row[0], intersectX, intersectY, row[1] + (xSecLineLength * c), row[2] + (xSecLineLength * s)])
 
+arcpy.AddField_management(xSecResultsTable, "ID", "LONG")
+arcpy.AddField_management(xSecResultsTable, "FROM_X", "DOUBLE")
+arcpy.AddField_management(xSecResultsTable, "FROM_Y", "DOUBLE")
+arcpy.AddField_management(xSecResultsTable, "TO_X", "DOUBLE")
+arcpy.AddField_management(xSecResultsTable, "TO_Y", "DOUBLE")
+
+fieldNameList = ["ID", "FROM_X", "FROM_Y", "TO_X", "TO_Y"]
+
+cursor = arcpy.da.InsertCursor(xSecResultsTable, (fieldNameList))
 for row in clRowVal:
-    row.append((intersectY-row[1])/(intersectX-row[0]))
+    cursor.insertRow(row)
+
+arcpy.XYToLine_management(xSecResultsTable,xSecResults,
+                         "FROM_X","FROM_Y","TO_X",
+                         "TO_Y","","",xSecPoints)
