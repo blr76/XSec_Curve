42bdef7cb1fafebcf005838dcb3cecf8978dcb8d
blr76 <37673419+blr76@users.noreply.github.com>
2019-04-25 17:58:04 -0500

    Update XSec_Curve.py

--- XSec_Curve.py
+++ XSec_Curve.py
@@ -6,23 +6,23 @@ import arcpy, math
 # x_sec_center_points = arcpy.GetParameterAsText(3)
 
 xSecPoints = r"E:\Trinity_River\Trinity_Cleanup\XsecTEST\T1_Transect_Points_Z_WSE.shp"
-xSecStartID = 485
-xSecEndID= 523
+xSecStartID = 123
+xSecEndID= 150
 xSecCenterPoints = r"E:\Trinity_River\Trinity_Cleanup\XsecTEST\T1_Q2002.shp"
 xSecLineLength = 200
 
 xSecPointsStart = r'in_memory\ptsStart'
 xSecPointsEnd = r'in_memory\ptsEnd'
-startMaxAttributes = [0,0]
-startMinAttributes =[999999,0]
-endMaxAttributes = [0,0]
-endMinAttributes =[999999,0]
+startMaxAttributes = [0,0,0]
+startMinAttributes =[999999,0,0]
+endMaxAttributes = [0,0,0]
+endMinAttributes =[999999,0,0]
 clRowVal = []
 xSecResultsTable = arcpy.CreateTable_management("in_memory", "xSecResultsTable")
 xSecResults = r"in_memory\xsSecResults"
 
 arcpy.Select_analysis(xSecPoints, xSecPointsStart, "ID = " + str(xSecStartID))
-cursor = arcpy.da.SearchCursor(xSecPointsStart, ['POINT_X', 'POINT_Y'])
+cursor = arcpy.da.SearchCursor(xSecPointsStart, ['POINT_X', 'POINT_Y', 'FID'])
 for row in cursor:
     if row[0] > startMaxAttributes[0]:
         startMaxAttributes = row
@@ -30,17 +30,28 @@ for row in cursor:
         startMinAttributes = row
 
 arcpy.Select_analysis(xSecPoints, xSecPointsEnd, "ID = " + str(xSecEndID))
-cursor = arcpy.da.SearchCursor(xSecPointsEnd, ['POINT_X', 'POINT_Y'])
+cursor = arcpy.da.SearchCursor(xSecPointsEnd, ['POINT_X', 'POINT_Y', 'FID'])
 for row in cursor:
     if row[0] > endMaxAttributes[0]:
         endMaxAttributes = row
     if row[0] < endMinAttributes[0]:
         endMinAttributes = row
 
-xa, ya = startMaxAttributes
-xb, yb = startMinAttributes
-xc, yc = endMaxAttributes
-xd, yd = endMinAttributes
+# xa, ya = startMaxAttributes
+# xb, yb = startMinAttributes
+# xc, yc = endMaxAttributes
+# xd, yd = endMinAttributes
+
+###DEBUG###
+xa, ya, f = startMaxAttributes
+xb, yb, f = startMinAttributes
+xc, yc, f = endMaxAttributes
+xd, yd, f = endMinAttributes
+print('startMaxAttributes' + str(startMaxAttributes))
+print('startMinAttributes' + str(startMinAttributes))
+print('endMaxAttributes' + str(endMaxAttributes))
+print('endMinAttributes' + str(endMinAttributes))
+###DEBUG###
 
 intersectX = ((yc-xc*((yd-yc)/(xd-xc)))-(ya-xa*((yb-ya)/(xb-xa))))/(((yb-ya)/(xb-xa))-((yd-yc)/(xd-xc)))
 intersectY = ya-xa*((yb-ya)/(xb-xa))+intersectX*((yb-ya)/(xb-xa))
@@ -58,15 +69,18 @@ with arcpy.da.SearchCursor(xSecCenterPoints, ["ID", "SHAPE@X", "SHAPE@Y"], "ID >
         m = ((intersectY - row[2]) / (intersectX - row[1]))
         c = 1 / math.sqrt(1 + math.pow(m,2))
         s =  m / math.sqrt(1 + math.pow(m,2))
-        clRowVal.append([row[0], intersectX, intersectY, row[1] + (xSecLineLength * c), row[2] + (xSecLineLength * s)])
+        clRowVal.append([row[0], intersectX, intersectY, row[1] + (xSecLineLength * c), row[2] + (xSecLineLength * s),m,c,s])
 
 arcpy.AddField_management(xSecResultsTable, "ID", "LONG")
 arcpy.AddField_management(xSecResultsTable, "FROM_X", "DOUBLE")
 arcpy.AddField_management(xSecResultsTable, "FROM_Y", "DOUBLE")
 arcpy.AddField_management(xSecResultsTable, "TO_X", "DOUBLE")
 arcpy.AddField_management(xSecResultsTable, "TO_Y", "DOUBLE")
+arcpy.AddField_management(xSecResultsTable, "m", "DOUBLE")
+arcpy.AddField_management(xSecResultsTable, "c", "DOUBLE")
+arcpy.AddField_management(xSecResultsTable, "s", "DOUBLE")
 
-fieldNameList = ["ID", "FROM_X", "FROM_Y", "TO_X", "TO_Y"]
+fieldNameList = ["ID", "FROM_X", "FROM_Y", "TO_X", "TO_Y", "m","c","s"]
 
 cursor = arcpy.da.InsertCursor(xSecResultsTable, (fieldNameList))
 for row in clRowVal:
@@ -74,4 +88,4 @@ for row in clRowVal:
 
 arcpy.XYToLine_management(xSecResultsTable,xSecResults,
                          "FROM_X","FROM_Y","TO_X",
-                         "TO_Y","","",xSecPoints)
+                         "TO_Y","","ID",xSecPoints)
